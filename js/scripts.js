8c6c79701a54736ad237248fdc6686960725b159
zraziu <zraziu@mail.ru>
2016-03-17 23:33:31 +0300

    Подставил свои значения

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+    $("#id_form").submit(function() { //устанавливаем событие отправки для формы с id=form
             var form_data = $(this).serialize(); //собераем все данные из формы
             $.ajax({
             type: "POST", //Метод отправки
-            url: "send.php", //путь до php фаила отправителя
+            url: "../mailto.php", //путь до php фаила отправителя
             data: form_data,
             success: function() {
                    //код в этом блоке выполняется при успешной отправке сообщения
