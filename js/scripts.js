2ea80b21f9143b47a78108a77cd0a09a2267bc3b
zraziu <zraziu@mail.ru>
2016-03-17 23:27:26 +0300

    Добавил код из примера https://habrahabr.ru/sandbox/93363/

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+            var form_data = $(this).serialize(); //собераем все данные из формы
+            $.ajax({
+            type: "POST", //Метод отправки
+            url: "send.php", //путь до php фаила отправителя
+            data: form_data,
+            success: function() {
+                   //код в этом блоке выполняется при успешной отправке сообщения
+                   alert("Ваше сообщение отпрвлено!");
+            });
+    });
+});    
+
+
+
+$(document).ready(function(){
 	var days = {
 	"2/7/2015":"",
 	"3/7/2015":"",
